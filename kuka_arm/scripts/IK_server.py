12438f0451d73b9c040ee1ded8f1f4fc956e7b7a
hemingchenhc@gmail.com <lzyytxd1>
2018-05-22 06:39:33 -0700

    now it saves symbolic calculation results for future use to speed up a new run

--- kuka_arm/scripts/IK_server.py
+++ kuka_arm/scripts/IK_server.py
@@ -18,6 +18,8 @@ from geometry_msgs.msg import Pose
 from mpmath import *
 from sympy import *
 import numpy as np
+import os
+import pickle
 
 
 class ForwardKinematicsSolver:
@@ -26,6 +28,8 @@ class ForwardKinematicsSolver:
     """
 
     def __init__(self):
+        output_dir = "../saved_data"
+
         #########################################################################################
         # Create symbols, remember slices do not include the end value.
         #########################################################################################
@@ -50,101 +54,133 @@ class ForwardKinematicsSolver:
         #########################################################################################
         # 1: homogeneous transforms between bas_link and gripper_link (end effector)
         #########################################################################################
-        # @formatter:off
-        T0_1 = Matrix([[             cos(q1),            -sin(q1),            0,              a0],
-                       [ sin(q1)*cos(alpha0), cos(q1)*cos(alpha0), -sin(alpha0), -sin(alpha0)*d1],
-                       [ sin(q1)*sin(alpha0), cos(q1)*sin(alpha0),  cos(alpha0),  cos(alpha0)*d1],
-                       [                   0,                   0,            0,               1]])
-        T0_1 = T0_1.subs(s)
+        if os.path.exists(os.path.join(output_dir, "T0_3.p")) and os.path.exists(os.path.join(output_dir, "T0_7.p")):
+            T0_3 = pickle.load(open(os.path.join(output_dir, "T0_3.p"), "rb"))
+            T0_7 = pickle.load(open(os.path.join(output_dir, "T0_7.p"), "rb"))
+        else:
+            # @formatter:off
+            T0_1 = Matrix([[             cos(q1),            -sin(q1),            0,              a0],
+                           [ sin(q1)*cos(alpha0), cos(q1)*cos(alpha0), -sin(alpha0), -sin(alpha0)*d1],
+                           [ sin(q1)*sin(alpha0), cos(q1)*sin(alpha0),  cos(alpha0),  cos(alpha0)*d1],
+                           [                   0,                   0,            0,               1]])
+            T0_1 = T0_1.subs(s)
 
-        T1_2 = Matrix([[             cos(q2),            -sin(q2),            0,              a1],
-                       [ sin(q2)*cos(alpha1), cos(q2)*cos(alpha1), -sin(alpha1), -sin(alpha1)*d2],
-                       [ sin(q2)*sin(alpha1), cos(q2)*sin(alpha1),  cos(alpha1),  cos(alpha1)*d2],
-                       [                   0,                   0,            0,               1]])
-        T1_2 = T1_2.subs(s)
+            T1_2 = Matrix([[             cos(q2),            -sin(q2),            0,              a1],
+                           [ sin(q2)*cos(alpha1), cos(q2)*cos(alpha1), -sin(alpha1), -sin(alpha1)*d2],
+                           [ sin(q2)*sin(alpha1), cos(q2)*sin(alpha1),  cos(alpha1),  cos(alpha1)*d2],
+                           [                   0,                   0,            0,               1]])
+            T1_2 = T1_2.subs(s)
 
-        T2_3 = Matrix([[             cos(q3),            -sin(q3),            0,              a2],
-                       [ sin(q3)*cos(alpha2), cos(q3)*cos(alpha2), -sin(alpha2), -sin(alpha2)*d3],
-                       [ sin(q3)*sin(alpha2), cos(q3)*sin(alpha2),  cos(alpha2),  cos(alpha2)*d3],
-                       [                   0,                   0,            0,               1]])
-        T2_3 = T2_3.subs(s)
+            T2_3 = Matrix([[             cos(q3),            -sin(q3),            0,              a2],
+                           [ sin(q3)*cos(alpha2), cos(q3)*cos(alpha2), -sin(alpha2), -sin(alpha2)*d3],
+                           [ sin(q3)*sin(alpha2), cos(q3)*sin(alpha2),  cos(alpha2),  cos(alpha2)*d3],
+                           [                   0,                   0,            0,               1]])
+            T2_3 = T2_3.subs(s)
 
-        T3_4 = Matrix([[             cos(q4),            -sin(q4),            0,              a3],
-                       [ sin(q4)*cos(alpha3), cos(q4)*cos(alpha3), -sin(alpha3), -sin(alpha3)*d4],
-                       [ sin(q4)*sin(alpha3), cos(q4)*sin(alpha3),  cos(alpha3),  cos(alpha3)*d4],
-                       [                   0,                   0,            0,               1]])
-        T3_4 = T3_4.subs(s)
+            T3_4 = Matrix([[             cos(q4),            -sin(q4),            0,              a3],
+                           [ sin(q4)*cos(alpha3), cos(q4)*cos(alpha3), -sin(alpha3), -sin(alpha3)*d4],
+                           [ sin(q4)*sin(alpha3), cos(q4)*sin(alpha3),  cos(alpha3),  cos(alpha3)*d4],
+                           [                   0,                   0,            0,               1]])
+            T3_4 = T3_4.subs(s)
 
-        T4_5 = Matrix([[             cos(q5),            -sin(q5),            0,              a4],
-                       [ sin(q5)*cos(alpha4), cos(q5)*cos(alpha4), -sin(alpha4), -sin(alpha4)*d5],
-                       [ sin(q5)*sin(alpha4), cos(q5)*sin(alpha4),  cos(alpha4),  cos(alpha4)*d5],
-                       [                   0,                   0,            0,               1]])
-        T4_5 = T4_5.subs(s)
+            T4_5 = Matrix([[             cos(q5),            -sin(q5),            0,              a4],
+                           [ sin(q5)*cos(alpha4), cos(q5)*cos(alpha4), -sin(alpha4), -sin(alpha4)*d5],
+                           [ sin(q5)*sin(alpha4), cos(q5)*sin(alpha4),  cos(alpha4),  cos(alpha4)*d5],
+                           [                   0,                   0,            0,               1]])
+            T4_5 = T4_5.subs(s)
 
-        T5_6 = Matrix([[             cos(q6),            -sin(q6),            0,              a5],
-                       [ sin(q6)*cos(alpha5), cos(q6)*cos(alpha5), -sin(alpha5), -sin(alpha5)*d6],
-                       [ sin(q6)*sin(alpha5), cos(q6)*sin(alpha5),  cos(alpha5),  cos(alpha5)*d6],
-                       [                   0,                   0,            0,               1]])
-        T5_6 = T5_6.subs(s)
+            T5_6 = Matrix([[             cos(q6),            -sin(q6),            0,              a5],
+                           [ sin(q6)*cos(alpha5), cos(q6)*cos(alpha5), -sin(alpha5), -sin(alpha5)*d6],
+                           [ sin(q6)*sin(alpha5), cos(q6)*sin(alpha5),  cos(alpha5),  cos(alpha5)*d6],
+                           [                   0,                   0,            0,               1]])
+            T5_6 = T5_6.subs(s)
 
-        T6_7 = Matrix([[             cos(q7),            -sin(q7),            0,              a6],
-                       [ sin(q7)*cos(alpha6), cos(q7)*cos(alpha6), -sin(alpha6), -sin(alpha6)*d7],
-                       [ sin(q7)*sin(alpha6), cos(q7)*sin(alpha6),  cos(alpha6),  cos(alpha6)*d7],
-                       [                   0,                   0,            0,               1]])
-        T6_7 = T6_7.subs(s)
-        # @formatter:on
+            T6_7 = Matrix([[             cos(q7),            -sin(q7),            0,              a6],
+                           [ sin(q7)*cos(alpha6), cos(q7)*cos(alpha6), -sin(alpha6), -sin(alpha6)*d7],
+                           [ sin(q7)*sin(alpha6), cos(q7)*sin(alpha6),  cos(alpha6),  cos(alpha6)*d7],
+                           [                   0,                   0,            0,               1]])
+            T6_7 = T6_7.subs(s)
+            # @formatter:on
 
-        # Transform from base_link to other joints
-        T0_3 = simplify(T0_1 * T1_2 * T2_3)
+            # Transform from base_link to other joints
+            T0_3 = simplify(T0_1 * T1_2 * T2_3)
 
-        # Transform from base link to gripper_link (end effector) - intrinsic
-        # T0_7 = simplify(T0_1 * T1_2 * T2_3 * T3_4 * T4_5 * T5_6 * T6_7)
-        T0_7 = simplify(T0_1 * T1_2 * T2_3 * T3_4 * T4_5 * T5_6 * T6_7)
+            # Transform from base link to gripper_link (end effector) - intrinsic
+            # T0_7 = simplify(T0_1 * T1_2 * T2_3 * T3_4 * T4_5 * T5_6 * T6_7)
+            T0_7 = simplify(T0_1 * T1_2 * T2_3 * T3_4 * T4_5 * T5_6 * T6_7)
+
+            # Save
+            pickle.dump(T0_3, open(os.path.join(output_dir, "T0_3.p"), "wb"))
+            pickle.dump(T0_7, open(os.path.join(output_dir, "T0_7.p"), "wb"))
 
         #########################################################################################
         # 2: roll, pitch and yaw rotation between base_link and gripper_link
         #########################################################################################
         roll, pitch, yaw = symbols("roll pitch yaw")
 
-        # @formatter:off
-        # Rotation about X
-        R_X = Matrix([[          1,           0,          0],
-                      [          0,   cos(roll), -sin(roll)],
-                      [          0,   sin(roll), cos(roll)]])
-        # Rotation about Y
-        R_Y = Matrix([[ cos(pitch),           0, sin(pitch)],
-                      [          0,           1,          0],
-                      [-sin(pitch),           0, cos(pitch)]])
-        # Rotation about Z
-        R_Z = Matrix([[   cos(yaw),   -sin(yaw),          0],
-                      [   sin(yaw),    cos(yaw),          0],
-                      [          0,           0,          1]])
-        # @formatter:on
+        if os.path.exists(os.path.join(output_dir, "R0_7.p")):
+            R0_7 = pickle.load(open(os.path.join(output_dir, "R0_7.p"), "rb"))
+        else:
+            # @formatter:off
+            # Rotation about X
+            R_X = Matrix([[          1,           0,          0],
+                          [          0,   cos(roll), -sin(roll)],
+                          [          0,   sin(roll), cos(roll)]])
+            # Rotation about Y
+            R_Y = Matrix([[ cos(pitch),           0, sin(pitch)],
+                          [          0,           1,          0],
+                          [-sin(pitch),           0, cos(pitch)]])
+            # Rotation about Z
+            R_Z = Matrix([[   cos(yaw),   -sin(yaw),          0],
+                          [   sin(yaw),    cos(yaw),          0],
+                          [          0,           0,          1]])
 
-        R0_7 = R_Z * R_Y * R_X
+            R0_7 = R_Z * R_Y * R_X
+            # @formatter:on
+
+            # Save
+            pickle.dump(R0_7, open(os.path.join(output_dir, "R0_7.p"), "wb"))
 
         #########################################################################################
         # Apply corrections due to gripper link coordinate frame has different definition
         # in URDF file and DH parameters
         #########################################################################################
-        # @formatter:off
-        T_z = Matrix([[    cos(np.pi), -sin(np.pi),             0, 0],
-                      [    sin(np.pi),  cos(np.pi),             0, 0],
-                      [             0,           0,             1, 0],
-                      [             0,           0,             0, 1]])
+        if os.path.exists(os.path.join(output_dir, "T_correction.p")) and \
+                os.path.exists(os.path.join(output_dir, "R_correction.p")):
+            T_correction = pickle.load(open(os.path.join(output_dir, "T_correction.p"), "rb"))
+            R_correction = pickle.load(open(os.path.join(output_dir, "R_correction.p"), "rb"))
+        else:
+            # @formatter:off
+            T_z = Matrix([[    cos(np.pi), -sin(np.pi),             0, 0],
+                          [    sin(np.pi),  cos(np.pi),             0, 0],
+                          [             0,           0,             1, 0],
+                          [             0,           0,             0, 1]])
 
-        T_y = Matrix([[ cos(-np.pi/2),           0, sin(-np.pi/2), 0],
-                      [             0,           1,             0, 0],
-                      [-sin(-np.pi/2),           0, cos(-np.pi/2), 0],
-                      [             0,           0,             0, 1]])
+            T_y = Matrix([[ cos(-np.pi/2),           0, sin(-np.pi/2), 0],
+                          [             0,           1,             0, 0],
+                          [-sin(-np.pi/2),           0, cos(-np.pi/2), 0],
+                          [             0,           0,             0, 1]])
+            # @formatter:on
 
-        T_correction = simplify(T_z * T_y)
-        R_correction = simplify(T_z[0:3, 0:3] * T_y[0:3, 0:3])
-        # @formatter:on
+            T_correction = simplify(T_z * T_y)
+            R_correction = simplify(T_z[0:3, 0:3] * T_y[0:3, 0:3])
+
+            # Save
+            pickle.dump(T_correction, open(os.path.join(output_dir, "T_correction.p"), "wb"))
+            pickle.dump(R_correction, open(os.path.join(output_dir, "R_correction.p"), "wb"))
 
         # Apply correction term
-        T0_7_corrected = simplify(T0_7 * T_correction)
-        R0_7_corrected = simplify(R0_7 * R_correction)
+        if os.path.exists(os.path.join(output_dir, "T0_7_corrected.p")) and \
+                os.path.exists(os.path.join(output_dir, "T0_7_corrected.p")):
+            T0_7_corrected = pickle.load(open(os.path.join(output_dir, "T0_7_corrected.p"), "rb"))
+            R0_7_corrected = pickle.load(open(os.path.join(output_dir, "R0_7_corrected.p"), "rb"))
+        else:
+            T0_7_corrected = simplify(T0_7 * T_correction)
+            R0_7_corrected = simplify(R0_7 * R_correction)
+
+            # Save
+            pickle.dump(T0_7_corrected, open(os.path.join(output_dir, "T0_7_corrected.p"), "wb"))
+            pickle.dump(R0_7_corrected, open(os.path.join(output_dir, "R0_7_corrected.p"), "wb"))
 
         # Put together member variables
         self.q1 = q1
